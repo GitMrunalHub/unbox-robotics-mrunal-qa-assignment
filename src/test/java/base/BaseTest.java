0a332d232d27ec718613d0ae88b0d474ede77f80
GitMrunalHub <66409976+GitMrunalHub@users.noreply.github.com>
2026-05-12 17:07:14 +0530

    Update BaseTest.java

--- src/test/java/base/BaseTest.java
+++ src/test/java/base/BaseTest.java
@@ -1,1 +1,26 @@
+package base;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.*;
+
+import java.time.Duration;
+
+public class BaseTest {
+
+    public WebDriver driver;
+
+    @BeforeMethod
+    public void setup() {
+
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+        driver.get("https://www.flipkart.com");
+    }
+
+    @AfterMethod
+    public void teardown() {
+        driver.quit();
+    }
+}
