c705f3613a7f9af3a1ca7007089f54274da94dbf
GitMrunalHub <66409976+GitMrunalHub@users.noreply.github.com>
2026-05-12 17:51:27 +0530

    Create ProductPage.java

---
+++ src/test/java/pages/ProductPage.java
@@ -0,0 +1,26 @@
+package pages;
+
+import org.openqa.selenium.*;
+import org.testng.Assert;
+
+public class ProductPage {
+
+    WebDriver driver;
+
+    public ProductPage(WebDriver driver) {
+        this.driver = driver;
+    }
+
+    By addToCart =
+        By.xpath("//button[contains(text(),'Add to cart')]");
+
+    public void addProductToCart() {
+
+        driver.findElement(addToCart).click();
+
+        Assert.assertTrue(
+            driver.getPageSource()
+                  .contains("Go to Cart")
+        );
+    }
+}
