f3b634b9fc39a8b3dfd60161cf903c5651dfa627
GitMrunalHub <66409976+GitMrunalHub@users.noreply.github.com>
2026-05-12 17:35:02 +0530

    Create SearchPage.java

---
+++ src/test/java/pages/SearchPage.java
@@ -0,0 +1,47 @@
+package pages;
+
+import org.openqa.selenium.*;
+import org.testng.Assert;
+
+import java.util.List;
+
+public class SearchPage {
+
+    WebDriver driver;
+
+    public SearchPage(WebDriver driver) {
+        this.driver = driver;
+    }
+
+    By resultText = By.xpath("//span[contains(text(),'results')]");
+
+    By products = By.xpath("//div[contains(@data-id,'MOB')]");
+
+    By compareCheckboxes = By.xpath("//div[contains(@data-id,'MOB')]//label[.//input[@type='checkbox']]");
+
+    public void verifySearchResults() {  
+
+        String text = driver.findElement(resultText).getText();
+
+        Assert.assertTrue(text.contains("results"));
+    }
+
+    public String addProductsToCompare() {
+
+        List<WebElement> phones = driver.findElements(products);
+        List<WebElement> compare = driver.findElements(compareCheckboxes);
+
+        compare.get(9).click();
+        compare.get(10).click();
+
+        return phones.get(9).getText();
+    }
+
+    public void click10thPhone() {
+
+        List<WebElement> phones =
+            driver.findElements(products);
+
+        phones.get(9).click();
+    }
+}
